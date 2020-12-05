3b815f1a5de4f7d8e67d608889935d561bf69147
AfkanGus <af.kan@bk.ru>
2020-12-05 18:01:10 +0500

    2. Модель данных. [#393333]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Afkan Salman ");
+        student.setGr("Java beginner");
+        student.setDate("23.10.2020");
+        System.out.println("student" + "-" + student.getFio() + "," + " groupe " + student.getGr() + "," +
+                "  receipt date " + " - " + student.getDate());
+
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fio;
+    private String gr;
+    private String date;
+
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getGr() {
+        return gr;
+    }
+
+    public void setGr(String gr) {
+        this.gr = gr;
+    }
+
+    public String  getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
