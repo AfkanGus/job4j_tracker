f4d8153686be7bd6900aee3b0201645402222571
AfkanGus <af.kan@bk.ru>
2020-12-14 21:51:10 +0500

    8. Что такое валидация? [#393339]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,42 +5,47 @@ import java.util.Arrays;
 
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
+    private int id = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(ids++);
+        item.setId(id++);
         items[size++] = item;
         return item;
     }
 
-    //Получения списка всех заявок
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        Item[] clearedItems = new Item[size];
+        clearedItems = Arrays.copyOf(items, size);
+        return clearedItems;
+    }
+
+    public void showAll() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(items[i].toString());
+        }
+    }
+
+    public Item[] findByName(String key) {
+        Item[] clearedItems = new Item[size];
+        int filled = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                clearedItems[filled] = items[i];
+                filled++;
+            }
+        }
+        clearedItems = Arrays.copyOf(clearedItems, filled);
+        return clearedItems;
     }
 
     public Item findById(int id) {
-        /*Находим индекс*/
+        /* Находим индекс */
         int index = indexOf(id);
-        /*Если индекс найден возвращаем item,иначе null*/
+        /* Если индекс найден возвращаем item, иначе null */
         return index != -1 ? items[index] : null;
     }
 
-
-    //Получение списка по имени
-    public Item[] findByName(String key) {
-        Item[] findItems = new Item[size];
-        int digit = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i].getName().equals(key)) {
-                findItems[digit] = items[i];
-                digit++;
-            }
-        }
-        return Arrays.copyOf(findItems, digit);
-    }
-
-    //Метод,возвращающий index по id
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
@@ -52,43 +57,24 @@ public class Tracker {
         return rsl;
     }
 
-    //Метод замены заявки,удаляя старую добавлять новую заявку
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        //Запишем с валидацией
-        boolean rsl = index != -1;
-        if (rsl) {
+        if (index == -1) {
+            return false;
+        } else
             items[index] = item;
-            items[index].setId(id);
-        }
-        return rsl;
+        items[index].setId(id);
+        return true;
     }
 
-       /* if (index == -1) {
-            return false;
-        }
-        items[index] = item;
-        items[index].setId(id);
-        return true;*/
-
-
-    //Метод удаления заявкки
     public boolean delete(int id) {
         int index = indexOf(id);
-        //Запишем с валидацией
-        boolean rsl = index != -1;
-        if (rsl) {
-            System.arraycopy(items, index, items, index + 1, size - index);
-            items[size - 1] = null;
-            size--;
-        }
-        return rsl;
-    }
-       /* if (index == -1) {
+        if (index == -1) {
             return false;
         }
-        System.arraycopy(items, index, items, index + 1, size - index);
+        System.arraycopy(items, index, items, index + 1, size - index - 1);
         items[size - 1] = null;
         size--;
-        return true;*/
-}
+        return true;
+    }
+    }
