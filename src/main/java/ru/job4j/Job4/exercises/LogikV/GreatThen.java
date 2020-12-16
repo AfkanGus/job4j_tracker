2b3996e945aa51e65be418504966c09a16037ec6
AfkanGus <af.kan@bk.ru>
2020-12-16 18:52:38 +0500

    Exercises from Job4j

---
+++ src/main/java/ru/job4j/Job4/exercises/LogikV/GreatThen.java
@@ -0,0 +1,7 @@
+package ru.job4j.Job4.exercises.LogikV;
+
+public class GreatThen {
+    public static boolean chek(int first, int second) {
+        return first > second;
+    }
+}
---
+++ src/main/java/ru/job4j/Job4/exercises/LogikV/LessThen.java
@@ -0,0 +1,7 @@
+package ru.job4j.Job4.exercises.LogikV;
+
+public class LessThen {
+    public static boolean check(int first, int second) {
+        return first < second;
+    }
+}
