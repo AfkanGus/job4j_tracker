6c5e6bb74dc999fa4df6fca90d2e587a63608868
afkan_guseynov <af.kan@bk.ru>
2020-12-28 20:38:34 +0500

    7. Шаблон проектирования - Стратегия [#393276]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -3,6 +3,9 @@ package ru.job4j.strategy;
 public class Square implements Shape {
     public String draw(){
         String ln = System.lineSeparator();
-        return ln;
+        return    "+ + + +" + ln
+                + "+     +" + ln
+                + "+     +" + ln
+                + "+ + + +" + ln;
     }
 }
