f5efb5844974e6bf3bc3a3fb565fe749e9ca6019
afkan_guseynov <af.kan@bk.ru>
2021-01-01 19:58:32 +0500

    9.1 Зависимость от System.out [#393260]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,37 +2,41 @@ package ru.job4j.tracker;
 
 public final class StartUI {
 
+    private final Output out;
+    public StartUI(Output out){
+        this.out = out;
+    }
+
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            System.out.print("Select: ");
+            //System.out.print("Select: ");
             int select = Integer.parseInt(input.askStr("Select: "));
             UserAction action = actions[select];
             run = action.execute(input, tracker);
-
-
         }
     }
 
     private void showMenu(UserAction[] actions) {
         System.out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+            System.out.println(index + "." + actions[index].name());
         }
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new ShowAllAction(),
+                new CreateAction(output), new ShowAllAction(),
                 new ReplaceAction(), new FindNameAction(),
                 new FindIdAction(), new DeleteAction(),
                 new ExitAction()
 
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
 
