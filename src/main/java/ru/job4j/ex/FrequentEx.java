f00e79a80e16280e3ad63cf68cb1c3b6d8ea48c5
afkan_guseynov <af.kan@bk.ru>
2021-01-05 23:46:57 +0500

    0. Что такое исключение. [#393290]

---
+++ src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+import java.lang.module.FindException;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (int i = 0; i < shops.length; i++) {
+            System.out.println(shops[i]);
+        }
+    }
+}
+/*
+ * Мы помним, что последний элемент в массиве на 3 элемента будет 2,
+ * так как индексы начинаются с элемента 0. Это значит, что условие
+ * должно быть без включения последнего элемента.*/
