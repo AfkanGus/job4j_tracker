d0544fa23d819a0685f5d79fae510df82fbee2fa
afkan_guseynov <af.kan@bk.ru>
2021-01-06 01:13:43 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#393282]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N can't be less then 0");
+            }
+            int rsl = 1;
+            for (int i = 1; i <= n; i++) {
+                rsl *= i;
+            }
+            return rsl;
+        }
+    }
+
