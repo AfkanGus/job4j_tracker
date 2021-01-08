88c2f16221a1de1948a6cf2702f0846064d211ee
afkan_guseynov <af.kan@bk.ru>
2021-01-08 22:48:32 +0500

    1.1. Тесты на StartUI. [#393287]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,55 +7,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-/*   @Test
-    public void whenCreateItem() {
-        Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName(), is("Item name"));
-    }
-
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        *//* Добавим в tracker новую заявку *//*
-        Item item = tracker.add(new Item(0,"Replaced item"));
-        *//* Входные данные должны содержать ID добавленной заявки item.getId() *//*
-        String replacedName = "New item name";
-        Input in = new StubInput(
-                new String[] {"0","1",replacedName,"1"}
-        );
-        UserAction[] actions = {
-                new ReplaceAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        *//* Добавим в tracker новую заявку *//*
-        Item item = tracker.add(new Item(0,"Deleted item"));
-        *//* Входные данные должны содержать ID добавленной заявки item.getId() *//*
-        Input in = new StubInput(
-                new String[] {"0", "1", "1"}
-        );
-        UserAction[] actions = {
-                new DeleteAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertNull(tracker.findById(item.getId()));
-    }*/
-
     @Test
     public void whenExit() {
         Output out = new StubOutput();
@@ -146,6 +97,28 @@ public class StartUITest {
                         + "Menu." + ln
                         + "0.Find an item by id" + ln
                         + "1.Exit" + ln + ""));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0.Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0.Exit%n"
+                )
+        ));
     }
 }
 
