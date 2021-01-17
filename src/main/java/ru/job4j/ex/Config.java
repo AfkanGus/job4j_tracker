b4b9745f449a928a7cc1303bae2a9df2c3a4bfe2
afkan_guseynov <af.kan@bk.ru>
2021-01-17 17:37:17 +0500

    3. Замена if-else-throw на if-throw [#393280]

---
+++ src/main/java/ru/job4j/ex/Config.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Config {
+    public static void load(String path) {
+
+        if (path == null) {
+            throw new IllegalArgumentException("Path could not by null");
+        }
+        System.out.println("load config by" + path);
+
+    }
+
+
+    public static void main(String[] args) {
+        load("jdbc://Localhost:8080");
+    }
+
+}
