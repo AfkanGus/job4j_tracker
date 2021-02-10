d32394299feb09a55d781d3c603d9e6c1720224c
afkan_guseynov <af.kan@bk.ru>
2021-02-10 14:36:16 +0500

    2. Очередь с приоритетом на LinkedList [#393242]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /*
+     * найти index нужного элемента.
+     * */
+    public void put(Task task) {
+        int index = 0;
+        /*
+         * пройдемся по всем элементам, а индекс будет равен размеру всей коллекции
+         * .*/
+        for (Task element : tasks) {
+            /*Сравниваем приаритеты важности*/
+            if (task.getPriority() > element.getPriority()) {
+                index++;
+            }
+            break;
+        }
+        this.tasks.add(index, task);
+    }
+
+    /*
+     * Метод take возвращает первый элемент из списка и удаляет его.
+     * Эти действия мы делаем через метод poll().*/
+    public Task take() {
+        return tasks.poll();
+    }
+}
