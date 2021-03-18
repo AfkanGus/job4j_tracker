9ddca08bdfb44d7fc8e9c19986b8a663a9a86226
AfkanGus <af.kan@bk.ru>
2021-03-18 23:04:00 +0500

    6. Изменить программу Tracker из 2-го модуля [#393246]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -8,6 +8,9 @@ public class Tracker {
     private int id = 1;
 
 
+    /**
+     * @noinspection checkstyle:EmptyLineSeparator
+     */
     private Tracker() {
 
     }
@@ -57,137 +60,6 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (Item item : items) {
-            if (item.getId() == id) {
-                rsl = items.indexOf(item);
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public boolean replace(int id, Item item) {
-        boolean result = false;
-        for (Item i : items) {
-            if (i.getId() == id) {
-                items.set(items.indexOf(i), item);
-                item.setId(id);
-                result = true;
-            }
-        }
-        return result;
-    }
-
-    public boolean delete(int id) {
-        boolean result = false;
-        for (Item item : items) {
-            if (item.getId() == id) {
-                items.remove(item);
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-}
-/*
-
- */
-/*добавили final для запрета наследования*//*
-
-public final class Tracker {
-    */
-/*поле для ссылки на объект*//*
-
-    private static Tracker instance = null;
-    */
-/*private final Item[] items = new Item[100];*//*
-
-    private final List<Item> items = new ArrayList<>();
-
-    private int id = 1;
-*/
-/*
-    private int size = 0;
-*//*
-
-
- */
-/*создаем конструктор*//*
-
-    private Tracker() {
-
-    }
-
-    */
-/*стат. метод для получения ссылки на объект*//*
-
-    public static Tracker getInstance() {
-        if (instance == null) {
-            instance = new Tracker();
-        }
-        return instance;
-    }
-
-    public Item add(Item item) {
-        item.setId(id++);
-        //items[size++] = item;
-        items.add(item);
-        return item;
-    }
-
-    */
-/*public Item[] findAll() {
-        Item[] clearedItems = new Item[size];
-        clearedItems = Arrays.copyOf(items, size);
-        return clearedItems;
-    }*//*
-
-    public List<Item> findAll() {
-        return items;
-    }
-
-    public void showAll() {
-        for (int i = 0; i < size; i++) {
-            System.out.println(items[i].toString());
-        }
-    }
-
-    public List<Item> findByName(String key) {
-        ArrayList<Item> clearedItems = new ArrayList<Item>();
-        for (Item item : items) {
-            if (item.getName().equals(key)) {
-                clearedItems.add(item);
-            }
-        }
-        return clearedItems;
-    }
-
-    public Item findById(int id) {
-        */
-/* *//*
- */
-/* Находим индекс *//*
- */
-/*
-        int index = indexOf(id);
-        *//*
- */
-/* Если индекс найден возвращаем item, иначе null *//*
- */
-/*
-        return index != -1 ? items[index] : null;*//*
-
-        for (Item item : items) {
-            if (item.getId() == id) {
-                return item;
-            }
-        }
-        return null;
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
         for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId() == id) {
                 rsl = index;
@@ -200,21 +72,21 @@ public final class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index == -1) {
-            items.set(index, item);
-            item.setId(id);
-            // items[index] = item;
-            // items[index].setId(id);
-            return true;
+            return false;
         }
+        item.setId(id);
+        items.set(index, item);
+        return true;
+    }
 
-
-        public boolean delete (int id){
-            int index = indexOf(id);
-            if (index == -1) {
-                return false;
-            }
-            items.remove(index);
-            return true;
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
         }
-*/
+        items.remove(index);
+        return true;
+    }
+}
+
 
