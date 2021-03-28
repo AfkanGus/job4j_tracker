715fb9543c1b35e19cb7fa7b3e3aefb60079ab44
afkan_guseynov <af.kan@bk.ru>
2021-03-28 20:31:53 +0500

    2. Размер списка и добавление элементов.

---
+++ src/test/java/ru/job4j/ListArrayLIstExr/AddElementTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.ListArrayLIstExr;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AddElementTest {
+    @Test
+    public void addNewElement() {
+        List<String> list = new ArrayList<>();
+        list.add(0,"first");
+        list.add(0,"second");
+        list.add(0,"third");
+        boolean result = AddElement.addNewElement(list, "fourth");
+        assertThat(result, is(true));
+    }
+}
