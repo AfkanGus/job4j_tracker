aee92a14fa6917141c891e25328e40cf03048c0f
afkan_guseynov <af.kan@bk.ru>
2021-07-15 15:49:58 +0500

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
@@ -0,0 +1,23 @@
+package ru.job4j.function;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.*;
+
+public class FunctionInterfaceUsage {
+    public static void main(String[] args) {
+        List<String> list = List.of("one", "two", "three", "one", "two", "three");
+        Supplier<Set<String>> sup = () -> new HashSet<>(list);
+        BiConsumer<Integer, String> consumer = (s, s1) -> System.out.println(s + s1);
+        Set<String> strings = sup.get();
+        int q = 1;
+        for (String s : strings) {
+            consumer.accept(q++, " is" + s);
+        }
+
+        BiFunction<String, Integer, String> biFunc = (s, i) -> s.concat(" ".concat(i.toString()));
+        System.out.println("Результат работы бифункции: " + biFunc.apply("Name", 123));
+        System.out.println("Результат работы бифункции: " + biFunc.apply("String number", 123));
+    }
+}
