48414f32baaafee3083514b8a53c8013b0b0304d
afkan_guseynov <af.kan@bk.ru>
2021-08-08 15:32:47 +0500

    1.3. Получить значения отображения по ключу.

---
+++ src/main/java/ru/job4j/map/KeySet.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.3 Получть значения  отображения по ключу.
+ */
+public class KeySet {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>(
+                Map.of(1, "root@root",
+                        2, "local@local",
+                        3, "host@host"
+                )
+        );
+        for (int key : map.keySet()) {
+            System.out.println(key + " - " + map.get(key));
+        }
+    }
+}
