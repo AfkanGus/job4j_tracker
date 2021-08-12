b961e77acd3c33034e08f72ddb3f1bc26782c7fb
afkan_guseynov <af.kan@bk.ru>
2021-08-12 09:53:30 +0500

    1.8. Объединение значений в Map.

---
+++ src/main/java/ru/job4j/map/Merge.java
@@ -0,0 +1,61 @@
+package ru.job4j.map;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * 1.8 Объединение значений в Map
+ * merge() - Если нужно объединить значения Map  с другим
+ */
+public class Merge {
+    public static Map<Integer, String> collectData(Map<Integer, String> names, List<User> users) {
+        for (User user : users) {
+            names.putIfAbsent(user.getId(), user.getName());
+            names.merge(user.getId(), user.getSurname(), (oldV, newV) -> oldV + " " + newV);
+        }
+        return names;
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+        private String surname;
+
+        public User(int id, String name, String surname) {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return id == user.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+}
