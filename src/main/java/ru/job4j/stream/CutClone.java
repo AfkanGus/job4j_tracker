33e513e2844b4dee2fc0b2774b30b0d3cf2f45e4
afkan_guseynov <af.kan@bk.ru>
2021-12-02 21:33:51 +0500

    Уникальность элементов

--- src/main/java/ru/job4j/stream/CutClone.java
+++ src/main/java/ru/job4j/stream/CutClone.java
@@ -1,2 +1,16 @@
-package ru.job4j.stream;public class CutClone {
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CutClone {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Ivan");
+        names.add("Stepan");
+        names.add("Ivan");
+        names.stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
 }
