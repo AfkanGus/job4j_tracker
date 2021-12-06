2ac61e390fd3333290b27b65483a8a677524e8dc
AfkanGus <af.kan@bk.ru>
2021-12-07 00:30:57 +0500

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/ConvertListTest.java
+++ src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -13,9 +13,9 @@ public class ConvertListTest {
     @Test
     public void whenTwoList() {
         List<int[]> in = new ArrayList<>();
-        in.add(new int[] {1});
-        in.add(new int[] {2, 3});
-        List<Integer> expect = Arrays.asList(1, 2, 3);
+        in.add(new int[]{1});
+        in.add(new int[]{2, 3});
+        List<Integer> expect = List.of(1, 2, 3);
         System.out.println(expect);
         assertThat(ConvertList.convert(in), is(expect));
     }
