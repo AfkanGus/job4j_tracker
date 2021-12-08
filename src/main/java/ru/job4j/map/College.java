601c5f0afecbff0aa46d6da2501f3a191ef45027
afkan_guseynov <af.kan@bk.ru>
2021-12-09 01:30:52 +0500

    3. Optional в банковских переводах. [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -2,9 +2,11 @@ package ru.job4j.map;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.Optional;
 
 /**
  * Class College - колледж, где ищется студент по аккаунту, и предмет по аккуанту и названии
+ * import java.util.Optional - класс обертка на null
  */
 public class College {
     private final Map<Student, Set<Subject>> students;
@@ -20,32 +22,36 @@ public class College {
      * @param - .filter(s -> s.getAccount().equals(account)) - фильтрует stream,if return true
      * @param - .findFirst() - метод возвращает результат в виде объекта типа Optional
      * @param - .orElse() -возвращает значение если в объекте Optional есть значение,
-     * @return - null - вернет пустую ссылку.
+     * @return - student - вернет пустую ссылку.
      */
-    public Student findByAccount(String account) {
-        return students.keySet()
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> student = students.keySet()
                 .stream()
                 .filter(s -> s.getAccount().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
+        return student;
     }
 
     /**
      * Метод - findBySubjectName - поиск предмета -Subject, по аккаунту и названию
+     * @ Optional.empty() - метод, что бы обернуть null
+     * @ student.isPresent() - проверка, что объект не null
+     * @ studentA.get() - получаем значение объекта
      */
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
-            return students.get(a)
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> studentA = findByAccount(account);
+        if (studentA.isPresent()) {
+            Optional<Subject> subject = students.get(studentA.get())
                     .stream()
                     .filter(s -> s.getName().equals(name))
-                    .findFirst()
-                    .orElse(null);
+                    .findFirst();
+            return subject;
+
         }
-        return null;
+        return Optional.empty();
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
                 Set.of(
                         new Subject("Math", 70),
@@ -53,10 +59,10 @@ public class College {
                 )
         );
         College college = new College(students);
-        Student student = college.findByAccount("000001");
+        Optional<Student> student = college.findByAccount("000001");
         System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.getScore());
+        Optional<Subject> english = college.findBySubjectName("000001", "English");
+        System.out.println("Оценка по найденному предмету: " + english.get().getScore());
 
     }
 }
