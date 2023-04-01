00cbd25f3775f85d3d02f83297a9b74a1e862b82
afkan_guseynov <af.kan@bk.ru>
2023-04-01 20:17:35 +0400

    удален

--- src/main/java/ru/job4j/upcastingVehicle/Plane.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.upcastingvehicle;
-
-public class Plane implements Vehicle {
-    @Override
-    public void move() {
-        System.out.println(getClass().getSimpleName()
-                + " - Самолет летает высоко в небе.");
-    }
-}
