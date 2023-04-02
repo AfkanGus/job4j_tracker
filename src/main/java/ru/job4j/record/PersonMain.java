ee4e40357af7b5fbdf62d46e72a1183f7670368d
afkan_guseynov <af.kan@bk.ru>
2023-04-02 19:31:51 +0400

    11. Record [#504880]

---
+++ src/main/java/ru/job4j/record/PersonMain.java
@@ -0,0 +1,25 @@
+package ru.job4j.record;
+
+public class PersonMain {
+    public static void main(String[] args) {
+       // Person person = new Person("Иван", 30);
+       // System.out.println("Имя: " + person.getName() + ", Возраст: " + person.getAge());
+        //System.out.println(person);
+        PersonRecord record = new PersonRecord("Иван", 30);
+        /**
+         * Если всё же нужно изменить поле, то потребуется создать новый объект record,
+         * скопировать в него значения полей, которые не менялись, и указать новое значение
+         * в поле, которое нужно изменить
+         */
+        PersonRecord newRecord = new PersonRecord(record.name(), 35);
+        System.out.println("Имя: " + record.name() + ", Возраст: " + record.age());
+        System.out.println(record);
+        /**
+         * создаем еще один record с теми же полями и значениями и сравниваем их,
+         * в результате чего получаем true, то есть hashcode и equals в
+         * record тоже переопределены.
+         */
+        PersonRecord record1 = new PersonRecord("Иван", 30);
+        System.out.println(record.equals(record1));
+    }
+}
---
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -0,0 +1,7 @@
+package ru.job4j.record;
+
+/**
+ * Внутри PersonRecord полностью соответствует классу Person.
+ */
+public record PersonRecord(String name, int age) {
+}
