4b719467f0be9b27083721e3fc6b6f6817b8396d
afkan_guseynov <af.kan@bk.ru>
2023-04-07 18:44:56 +0400

    commit

--- src/main/java/ru/job4j/polumorph/Main.java
+++ src/main/java/ru/job4j/polumorph/Main.java
@@ -2,32 +2,18 @@ package ru.job4j.polumorph;
 
 /**
  * Создадим класс Main и выполним методы класса TownCar.
+ * <p>
+ * в классе Main типом переменной townCar является не TownCar,
+ * а Vehicle, то есть переменная типа Vehicle хранит
+ * ссылку на объект TownCar
+ * В случае подобного наследования интерфейсов мы теперь можем в методе main()
+ * объявить объект sportCar как тип всеобъемлющего интерфейса, то есть Vehicle,
+ * так как Vehicle наследует методы Fuel, а также определяет свои методы:
+ * Для вызова метода, определенного как static,
+ * нужно указать имя интерфейса и через точку название метода.
  */
 public class Main {
     public static void main(String[] args) {
-        /*
-         в классе Main типом переменной townCar является не TownCar,
-          а Vehicle, то есть переменная типа Vehicle хранит
-           ссылку на объект TownCar
-         *//*
-        Vehicle townCar = new TownCar();
-        townCar.changeGear();
-        townCar.accelerate();
-        townCar.steer();
-        townCar.brake();
-        *//*
-        Теперь переключим старую реализацию на новую.
-         *//*
-        Vehicle sportCar = new SportCar();
-        sportCar.changeGear();
-        sportCar.accelerate();
-        sportCar.steer();
-        sportCar.brake();*/
-        /*
-         * В случае подобного наследования интерфейсов мы теперь можем в методе main()
-         * объявить объект sportCar как тип всеобъемлющего интерфейса, то есть Vehicle,
-         * так как Vehicle наследует методы Fuel, а также определяет свои методы:
-         */
         SportCar sportCar = new SportCar();
         Vehicle vehicle = sportCar;
         Fuel fuel = sportCar;
@@ -36,11 +22,6 @@ public class Main {
         vehicle.steer();
         vehicle.brake();
         fuel.refill();
-        /*
-        Для вызова метода, определенного как static,
-         нужно указать имя интерфейса и через точку название метода.
-         */
         Vehicle.getDragCoefficient();
-
     }
 }
--- src/main/java/ru/job4j/record/PersonMain.java
+++ src/main/java/ru/job4j/record/PersonMain.java
@@ -1,12 +1,14 @@
 package ru.job4j.record;
 
+/**
+ * Person = new Person("Иван", 30)
+ * System.out.println("Имя: " + person.getName() + ", Возраст: " + person.getAge());
+ * System.out.println(person);
+ */
 public class PersonMain {
     public static void main(String[] args) {
-       // Person person = new Person("Иван", 30);
-       // System.out.println("Имя: " + person.getName() + ", Возраст: " + person.getAge());
-        //System.out.println(person);
         PersonRecord record = new PersonRecord("Иван", 30);
-        /**
+        /*
          * Если всё же нужно изменить поле, то потребуется создать новый объект record,
          * скопировать в него значения полей, которые не менялись, и указать новое значение
          * в поле, которое нужно изменить
@@ -14,7 +16,7 @@ public class PersonMain {
         PersonRecord newRecord = new PersonRecord(record.name(), 35);
         System.out.println("Имя: " + record.name() + ", Возраст: " + record.age());
         System.out.println(record);
-        /**
+        /*
          * создаем еще один record с теми же полями и значениями и сравниваем их,
          * в результате чего получаем true, то есть hashcode и equals в
          * record тоже переопределены.
