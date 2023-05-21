7c16c55fb5709d77a9e97a514b26f439e99158a0
afkan_guseynov <af.kan@bk.ru>
2023-05-21 23:57:25 +0400

    1. Генератор текста

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,19 +1,38 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
+        /*originWordsMap - это Map<String, Integer>,
+         где ключом является слово из оригинального текста,
+         а значением - количество вхождений этого слова.
+         */
         Map<String, Integer> originWordsMap = new HashMap<>();
-
         /* Создаем карту слов из оригинального текста*/
         String[] originWords = origin.split("\\s+");
         for (String word : originWords) {
-            String cleanedWord = word.replaceAll("[^a-zA-Zа-яА-Я]", "").toLowerCase();
+            String cleanedWord = word.replaceAll("[^a-zA-Zа-яА-Я]",
+                    "").toLowerCase();
             if (!cleanedWord.isEmpty()) {
-                originWordsMap.put(cleanedWord, originWordsMap.getOrDefault(cleanedWord, 0) + 1);
+                /*
+                put Добовляет ключ - cleanedWord -очищенное и приведенное к
+                нижнему регистру слово из оригинального текста. в мап.
+                 */
+                originWordsMap.put(cleanedWord,
+                        /*
+ этот метод получает значение, связанное с ключом cleanedWord из originWordsMap
+  Если ключ cleanedWord отсутствует в карте, то возвращается значение по умолчанию - 0;
+   Если ключ cleanedWord присутствует в карте, то возвращается связанное с ним значение.
+   после получения значения из карты, к нему добавляется 1. Таким образом,
+    мы увеличиваем счетчик вхождений слова cleanedWord в оригинальном тексте.
+    Если слово cleanedWord не было ранее добавлено в originWordsMap,
+     то при вызове метода originWordsMap.put(cleanedWord,
+      originWordsMap.getOrDefault(cleanedWord, 0) + 1); оно будет добавлено в карту со значением,
+       равным 1. Это означает, что у данного слова будет одно вхождение в оригинальном тексте.
+                         */
+                        originWordsMap.getOrDefault(cleanedWord, 0) + 1);
             }
         }
 
---
+++ src/main/java/ru/job4j/collection/WordFrequencyCounter.java
@@ -0,0 +1,40 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyCounter {
+    public static void main(String[] args) {
+        String text = "Lorem ipsum dolor sit amet";
+
+        Map<String, Integer> wordFrequencyMap = countWordFrequency(text);
+        /* Выводим результаты*/
+        for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countWordFrequency(String text) {
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+
+        /* Разделяем текст на отдельные слова*/
+        String[] words = text.split("\\s+");
+
+        // Подсчитываем частоту встречаемости слов
+        for (String word : words) {
+
+            /*Удаляем все символы, кроме букв, из слова*/
+
+            String cleanedWord = word.replaceAll("[^a-zA-Zа-яА-Я]", "");
+                    cleanedWord.toLowerCase();
+
+            if (!cleanedWord.isEmpty()) {
+                 /*Обновляем частоту встречаемости слова в карте*/
+                wordFrequencyMap.put(cleanedWord,
+                        wordFrequencyMap.getOrDefault(cleanedWord, 0) + 1);
+            }
+        }
+
+        return wordFrequencyMap;
+    }
+}
