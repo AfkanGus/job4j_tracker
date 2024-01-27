a559047eb2381b985ff9bdeadb9bcfc1388ebb7b
AfkanGus <af.kan@bk.ru>
2024-01-27 14:47:43 +0500

    9. Генерирование колоды карт. FlatMap [#504873 #476695].

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 /**
  * 9. Генерирование колоды карт. FlatMap [#504873 #476695].
  */
+
 import java.util.stream.Stream;
 
 enum Suit {
@@ -29,10 +30,18 @@ public class Card {
         return value;
     }
 
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
+    }
+
     public static void main(String[] args) {
         Stream.of(Suit.values())
                 .flatMap(suit1 -> Stream.of(Value.values())
                         .map(value1 -> new Card(suit1, value1)))
-                .forEach(card -> System.out.println(card.getSuit() + " " + card.getValue()));
+                .forEach(System.out::println);
     }
 }
