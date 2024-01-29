f694332f24c27639216507a985db693309d99ca9
AfkanGus <af.kan@bk.ru>
2024-01-29 23:14:55 +0500

    11. Методы forEach() и peek() [#504924]

--- src/main/java/ru/job4j/stream/ForEachExample.java
+++ src/main/java/ru/job4j/stream/ForEachExample.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * peek() - это промежуточная операция. Выполняет действие для каждого
@@ -10,13 +11,21 @@ import java.util.List;
  * <p>
  * forEach() - это конечная операция. Выполняет действие для каждого
  * элемента потока, завершая поток.
+ * акже метод peek() похож на метод map(). Разница между этими методами в том, что peek() просто преобразует элементы
+ * потока и возвращает поток из измененных элементов того же типа, а метод map() в процессе преобразования элементов
+ * может возвращать поток из элементов другого типа после изменения. Поэтому метод peek() чаще используется в
+ * отладочных целях для вывода состояния элемента в потоке, нежели для каких-либо преобразований.
  */
 public class ForEachExample {
     public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("odin", "Dva", "Tri");
-        stringList.stream() /*создаем стрим из листа*/
-                .map(String::toLowerCase) /*добавляем промежуточную информацию понижение регистра*/
-                .forEach(System.out::println); /*печатаем каждый измененный объект и закрывает поток*/
+        List<StringBuilder> names = Arrays.asList(
+                new StringBuilder("Mixail"), new StringBuilder("Ivan"), new StringBuilder("Elena"));
+        List<StringBuilder> stringBuilders = names
+                .stream()
+                .peek((e) -> e.append("(Ycenik)"))
+                .sorted()
+                .toList();
+        System.out.println(stringBuilders);
     }
 }
 
